cffb9027b4e16a5eb5bc205a122a50c9146ae2da
PSG-00 <cdjsdj1902@gmail.com>
2026-03-10 15:49:29 +0900

    누락된 파일 추가

---
+++ src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
@@ -0,0 +1,14 @@
+package com.sprint.mission.discodeit.storage;
+
+import com.sprint.mission.discodeit.dto.BinaryContentDto;
+import org.springframework.http.ResponseEntity;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+public interface BinaryContentStorage {
+    UUID put (UUID uuid, byte[] bytes);
+    InputStream get (UUID uuid);
+    ResponseEntity<?> download(BinaryContentDto.Response response);
+    void delete(UUID binaryContentId);
+}
